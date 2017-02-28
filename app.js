54eb227b2d24e5abf93b44e42da28c4d2c624084
MoodyKSA <moodyksa1@live.com>
2017-03-01 00:25:36 +0300

    at least we spawned something'

--- app.js
+++ app.js
@@ -8,8 +8,75 @@ app.use(express.static(__dirname+'/public'));
 app.get('/',function(req, res, send){res.sendfile('index.html');});
 server.listen(8080,function(){console.log(appname+'listening');});
 
-var eServer= new Eureca.Server();
+var connections = {};
+var eServer= new Eureca.Server({allow:['tchat.send','tchat.userConnected','tchat.getPos']});
 	eServer.attach(server);
 
-eServer.onConnect(function(connection){	});
-eServer.onDisconnect(function (connection){ });
+var tchatServer = eServer.exports.tchat = {};
+var Player = function(id,nick,x,y){
+	this.id = id;
+	this.nick = nick;
+	this.sprite = null;			
+}
+
+	// Login after we set up the nick
+	tchatServer.login = function(nick)
+	{
+		if(nick != undefined)
+		{
+			var id = this.connection.id;
+			connections[id].player = new Player(id,nick,0,0);
+			return {player:connections[id].player};
+		}
+	}
+
+	tchatServer.loggedIn = function()
+	{
+		for(var c in connections)
+		{
+			if(connections[c].player && connections[c].player.nick)
+			{
+				connections[c].client.tchat.userConnected();
+			}	
+		}
+	}
+
+	// Send message to all connected users
+	tchatServer.send = function(msg){
+		for(var c in connections)
+		{
+			if(connections[c].player && connections[c].player.nick)
+				connections[c].client.tchat.send(connections[this.connection.id].player.nick, msg);
+		}
+	}	
+
+	tchatServer.lookForOthers= function()
+	{
+		var others = [];
+		var id = connections[this.connection.id].player;	
+		if(!id) return;
+
+		for(var c in connections)
+		{
+			if(connections[c].player && connections[c].player.nick)
+			{
+				if(connections[c].player.nick != id.nick)
+				{
+					others.push(connections[c].player);
+				}
+			}	
+		}		
+		return others;
+	}
+
+eServer.onConnect(function(connection){	
+	connections[connection.id] = {
+		player:null,
+		client: eServer.getClient(connection.id)
+	};			
+	// Nothing here happens other than saving the connection into our array
+});
+
+eServer.onDisconnect(function (connection){ 
+	delete connections[connection.id];
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,119 @@
 var appname = 'main.js ';
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'phaser-example', { preload: preload, create: eClientCreate, update: update, render: render });
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 
+	'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 var eClient;
 var eServerProxy;
-var tchat;
+var sprite;
+var gArcade;
+var otherPlayers = [];
+var otherSprites = [];
+var player = null;
+var tchat; // Namespace 
 
-function preload(){}
-function eClientCreate()
+function preload()
+{
+	game.load.image('block', './assets/block.png');
+}
+
+function create()
 {
 	eClient = new Eureca.Client();
 	eClient.ready(function(serverProxy){
-		eServerProxy = serverProxy;
+		console.log(appname +' client side connection');		
+	
+		game.physics.startSystem(Phaser.Physics.ARCADE);
 
-		create();
-	});		
+		gArcade 		= game.physics.arcade;
+		eServerProxy 	= serverProxy;
+		tchat			= eClient.exports.tchat = {}; // Namespace
+
+		// First thing the user sees, DOM stuff
+		$('#nick').val('anon-'+new Date().getTime().toString(32)); 
+
+		// we can use chat now
+ 		$('#sendBtn').click(function() {
+  			if (!eServerProxy) return; 
+  			if (!player) return; 
+ 			eServerProxy.tchat.send($('#msg').val());
+			$('#msg').val('');
+ 		});
+
+ 		$('#logBtn').click(function() {
+
+  			if (!eServerProxy) return;   
+
+			// Login button clicked, lets handle it
+  			eServerProxy.tchat.login($('#nick').val()).onReady(function(result){
+
+				// Show chat menu
+				$('#auth').fadeOut('fast', function() {
+					$('#main').fadeIn('fast');	
+				});
+
+				// Player object created, lets give it sprite
+				player = result.player;
+				player.sprite = game.add.sprite(200,game.world.centerY+100,'block');
+				player.sprite.anchor.set(0.5);
+				game.physics.arcade.enable(player.sprite);
+				$(document).prop('title', player.nick);	
+				
+				// This player is all set.
+				eServerProxy.tchat.loggedIn().onReady(function(){
+					console.log('We are all set');
+				});
+			});
+ 		}); 
+	
+		tchat.send = function(nick, message){
+	  		var tchatline = $('<li><b>'+nick+' </b><span>'+message+'</span></li>');
+		  	$('#msgbox').append(tchatline);
+		}
+
+		tchat.getPos = function()
+		{
+			return {id:player.id, x:player.sprite.position.x, y:player.sprite.position.y};
+		}
+
+		tchat.userConnected = function()
+		{
+			console.log(player.nick + ' is searching for others...');
+			eServerProxy.tchat.lookForOthers().onReady(function(others){
+					otherPlayers = others;
+					console.log('Hey! A new user is logged! '+otherPlayers.length + ' found so far');
+					
+				for(var i =0; i < otherPlayers.length; i++)
+				{
+					var enemy = otherPlayers[i];
+					if(!enemy) return;
+					enemy.sprite = game.add.sprite(200,game.world.centerY+100,'block');
+					enemy.sprite.anchor.set(0.5);
+					game.physics.arcade.enable(enemy.sprite);
+
+					otherSprites.push(enemy);
+				}
+			});
+		}	
+	});	
 }
-function create(){}
-function render(){}
-function update(){}
 
+// when we get more players, the ID will be diferent in arrays so it would act funny 
+// when looping through them
+function update()
+{
+	if(player && player.sprite){
+		gArcade.moveToPointer(player.sprite,300);	
+		//console.log(player.sprite.position.x);
+	}
+
+	for(var i =0; i < otherPlayers.length; i++)
+	{
+		var enemy = otherPlayers[i];
+		if(!enemy) return;
+		
+		// go to server and get the new updated coords
+		// return id,x,y
+	}
+}
+
+function render(){}
