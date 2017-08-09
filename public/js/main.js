744823c01658fdc6635d87d423aebc6628b40a85
MoodyKSA <moodyksa1@live.com>
2017-08-09 19:32:47 +0300

    wat

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,51 @@
-var appname = 'main.js ';
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 
-	'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'test', null, true, false);
+var BasicGame = function(game) {}; // DIS IS A FUNCTION
+BasicGame.Boot = function(game) {};
 
-var eClient;
-var eServerProxy;
+var isoGroup, sorted;
+BasicGame.Boot.prototype =
+{
+	preload: function(){
+		game.load.image('cube', '../assets/cube.png');	
 
-function preload(){}
-function create(){
-	$(document).ready(function(){
-		console.log(appname +'JQuery ready');
-	});
+		game.time.advancedTiming = true;
+		game.plugins.add(new Phaser.Plugin.Isometric(game));
 
-	eClient = new Eureca.Client();
-	eClient.ready(function(serverProxy){
-		console.log(appname +'Eureca client ready');		
-		eServerProxy 	= serverProxy;	
-     }); 
-}
-function update(){}
-function render(){}
+		game.iso.anchor.setTo(0.5, 0.5);
+
+	},
+	create: function(){
+		isoGroup = game.add.group();
+
+		var degree = (Math.PI*2)/(32);
+		var cube;
+		for(var xx=0; xx < 32; xx++)
+		{
+			cube = game.add.isoSprite(	Math.sin(xx*degree)*240,
+										Math.cos(xx*degree)*240,0,'cube', 0, isoGroup);
+			cube.anchor.set(0.5,0.2) // ?
+			cube.oldZ = cube.z;
+			game.add.tween(cube).to(
+				{isoZ: 10}, 1000*((xx)%10), Phaser.Easing.Quadratic.InOut, true, 0, Infinity, true);
+		}
+
+		sorted = false;
+		game.input.onDown.add(function(){
+			sorted = !sorted;
+			if(sorted)
+				game.iso.simpleSort(isoGroup);
+			else
+				isoGroup.sort('oldZ');
+
+		}, this);
+	},
+	update: function(){
+
+	},
+	render: function(){
+		game.debug.text("I Moayad, the great me, has created this!", 2, 36, "#000000");
+	}
+};
+
+game.state.add('Boot', BasicGame.Boot);
+game.state.start('Boot');
